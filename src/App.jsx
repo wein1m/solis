a437590ad4c4178b9f697b506bf009ef7bd8c54a
wein1m <weynn.sz@gmail.com>
2026-08-23 15:22:04 +0800

    feat: added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./sections/Hero";
+
 const App = () => {
   return (
-    <div className="bg-red-500 text-5xl">
-      Hello World!
+    <div>
+      <Hero />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,32 @@
+import heroImg from "../assets/hero.jpg";
+
+const Hero = () => {
+  return (
+    <div className="flex py-24 px-42 w-full h-screen gap-12">
+      <div className="w-1/2 h-full">
+        <img src={heroImg} className="w-full h-full object-cover rounded-xl" />
+      </div>
+
+      <div className="w-1/2 flex flex-col justify-around">
+        <h1 className="font-arizona text-7xl text-center">
+          Bloom into the life you deserve with Solis
+        </h1>
+        <div className="font-figtree flex flex-col items-center text-center gap-6">
+          <p>
+            Professional tele-counseling and guided mental wellness programs,
+            designed to help you manage daily stress, overcome burnout, and
+            prioritize your well-being from the comfort of your home.
+          </p>
+          <a
+            href="#"
+            className="rounded-lg bg-primary text-white font-medium px-6 py-3 hover:bg-primary-hover"
+          >
+            Schedule a consultation
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
