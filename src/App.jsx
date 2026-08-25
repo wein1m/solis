e0214b8e54887d15cd259f124231647d47fbafd3
wein1m <weynn.sz@gmail.com>
2026-08-25 16:13:29 +0800

    chore: added lenis smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { ReactLenis } from "lenis/react";
+import "lenis/dist/lenis.css";
+
 import About from "./sections/About";
 import Hero from "./sections/Hero";
 import WhyUs from "./sections/WhyUs";
 
 const App = () => {
   return (
-    <div>
+    <ReactLenis
+      root
+      options={{
+        lerp: 0.08,
+        autoRaf: true,
+        anchors: true,
+      }}
+    >
       <Hero />
       <About />
       <WhyUs />
-    </div>
+    </ReactLenis>
   );
 };
 
