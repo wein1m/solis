3f0eac659128df383e913d77008ceb6844ba3b2a
wein1m <weynn.sz@gmail.com>
2026-08-30 16:48:45 +0800

    refactor: restructure codebase for multipage routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,13 @@
-import { ReactLenis } from "lenis/react";
-import "lenis/dist/lenis.css";
+import { Routes, Route } from "react-router-dom";
 
-import About from "./sections/About";
-import Hero from "./sections/Hero";
-import WhyUs from "./sections/WhyUs";
-import Experience from "./sections/Experience";
-import Testimonials from "./sections/Testimonials";
-import Prices from "./sections/Prices";
-import NavBar from "./sections/NavBar";
-import FAQ from "./sections/FAQ";
-import Footer from "./sections/Footer";
+import Home from "./pages/Home/Home";
 
-const App = () => {
+function App() {
   return (
-    <ReactLenis
-      root
-      options={{
-        lerp: 0.08,
-        autoRaf: true,
-        anchors: true,
-      }}
-    >
-      <NavBar />
-      <Hero />
-      <About />
-      <WhyUs />
-      <Experience />
-      <Testimonials />
-      <Prices />
-      <FAQ />
-      <Footer />
-    </ReactLenis>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
-};
+}
 
 export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,37 @@
+import { ReactLenis } from "lenis/react";
+import "lenis/dist/lenis.css";
+
+import About from "./sections/About";
+import Hero from "./sections/Hero";
+import WhyUs from "./sections/WhyUs";
+import Experience from "./sections/Experience";
+import Testimonials from "./sections/Testimonials";
+import Prices from "./sections/Prices";
+import NavBar from "./sections/NavBar";
+import FAQ from "./sections/FAQ";
+import Footer from "./sections/Footer";
+
+const Home = () => {
+  return (
+    <ReactLenis
+      root
+      options={{
+        lerp: 0.08,
+        autoRaf: true,
+        anchors: true,
+      }}
+    >
+      <NavBar />
+      <Hero />
+      <About />
+      <WhyUs />
+      <Experience />
+      <Testimonials />
+      <Prices />
+      <FAQ />
+      <Footer />
+    </ReactLenis>
+  );
+};
+
+export default Home;
