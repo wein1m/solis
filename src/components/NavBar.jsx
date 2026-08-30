9896ca439ace2bad2147878abd02271a324ffbe9
wein1m <weynn.sz@gmail.com>
2026-08-30 16:59:58 +0800

    refactor: move shared components to root dir

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import logo from "../assets/logo.png";
+
+const NavBar = () => {
+  return (
+    <nav className="sticky top-0 left-0 w-full z-999 bg-transparent backdrop-blur-xs py-6 px-52">
+      <div className="flex flex-row justify-between items-center">
+        <img src={logo} className="object-cover h-10"/>
+        {/* <h1 className="font-arizona text-4xl">Solis</h1> */}
+
+        <div className="flex flex-row gap-10 items-center">
+            <div className="flex flex-row gap-8 tracking-wider">
+                <a href="#">About</a>
+                <a href="#">Pricing</a>
+                <a href="#">Contact Us</a>
+            </div>
+            <a href="#" className="primary-cta">Book a Consultation</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,16 @@
 import { ReactLenis } from "lenis/react";
 import "lenis/dist/lenis.css";
 
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+
 import About from "./sections/About";
 import Hero from "./sections/Hero";
 import WhyUs from "./sections/WhyUs";
 import Experience from "./sections/Experience";
 import Testimonials from "./sections/Testimonials";
 import Prices from "./sections/Prices";
-import NavBar from "./sections/NavBar";
 import FAQ from "./sections/FAQ";
-import Footer from "./sections/Footer";
 
 const Home = () => {
   return (
